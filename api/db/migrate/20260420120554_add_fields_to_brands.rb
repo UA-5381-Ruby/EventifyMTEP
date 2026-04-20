6e270274e93740fa97f54bd3a1a84e2970924e87
tania-macheieva <macheieva.tetiana@gmail.com>
2026-04-20 13:00:32 +0000

    some fixes

--- api/db/migrate/20260420120554_add_fields_to_brands.rb
+++ api/db/migrate/20260420120554_add_fields_to_brands.rb
@@ -2,7 +2,8 @@
 
 class AddFieldsToBrands < ActiveRecord::Migration[8.1]
   def change
-    add_column :brands, :primary_color, :string, null: false
-    add_column :brands, :secondary_color, :string, null: false
+    add_column :brands, :primary_color, :string, null: false, default: '#3b82f6'
+    add_column :brands, :secondary_color, :string, null: false, default: '#1f2937'
+    add_index :brands, :subdomain, unique: true
   end
 end
