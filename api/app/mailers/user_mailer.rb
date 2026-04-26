ba3c708872a902df605fbe0d9bb30e932dfa72e3
Tania Macheieva <120329506+tania-macheieva@users.noreply.github.com>
2026-04-27 01:40:07 +0300

    Update reset password link to use frontend URL

--- api/app/mailers/user_mailer.rb
+++ api/app/mailers/user_mailer.rb
@@ -3,7 +3,8 @@
 class UserMailer < ApplicationMailer
   def reset_password(user, signed_id)
     @user = user
-    @reset_link = "https://app.com/reset?token=#{signed_id}"
+    frontend_url = ENV.fetch('FRONTEND_URL')
+    @reset_link = "#{frontend_url}/reset?token=#{signed_id}"
     mail(to: @user.email, subject: 'Reset your password')
   end
 end
