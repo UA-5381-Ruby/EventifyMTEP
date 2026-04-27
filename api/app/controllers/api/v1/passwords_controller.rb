2c7dd2b41f56a6c957e5b4e7114801102ac2bd9b
tania-macheieva <macheieva.tetiana@gmail.com>
2026-04-27 10:23:28 +0000

    lint fixes

--- api/app/controllers/api/v1/passwords_controller.rb
+++ api/app/controllers/api/v1/passwords_controller.rb
@@ -23,7 +23,10 @@ module Api
         user = User.find_by_token_for(:password_reset, params[:token])
 
         return render json: { error: 'Invalid or expired token' }, status: :bad_request if user.nil?
-        return render json: { error: 'New password cannot be blank' }, status: :unprocessable_content if params[:new_password].blank?
+        if params[:new_password].blank?
+          return render json: { error: 'New password cannot be blank' },
+                        status: :unprocessable_content
+        end
 
         if user.update(password: params[:new_password])
           render json: { message: 'Password successfully updated' }, status: :ok
