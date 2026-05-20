ca84e15f6c54b5c46e7d131c8809bfcf74e503ba
Liubomyr Halamaha <galamagal86@gmail.com>
2026-05-20 13:39:52 +0000

    update eslint.config.js add eslint-plugin-check-file

--- web/eslint.config.js
+++ web/eslint.config.js
@@ -5,6 +5,7 @@ import reactRefresh from 'eslint-plugin-react-refresh';
 import tseslint from 'typescript-eslint';
 import { defineConfig, globalIgnores } from 'eslint/config';
 import eslintConfigPrettier from 'eslint-config-prettier';
+import checkFile from 'eslint-plugin-check-file';
 
 export default defineConfig([
   globalIgnores(['dist']),
@@ -20,5 +21,33 @@ export default defineConfig([
       globals: globals.browser,
     },
   },
+
+  // Конфігурація для тотального kebab-case всередині папки src
+  {
+    files: ['src/**/*.{ts,tsx}'],
+    plugins: {
+      'check-file': checkFile,
+    },
+    rules: {
+      'check-file/filename-naming-convention': [
+        'error',
+        {
+          // Тепер абсолютно всі файли (.ts та .tsx) мають бути в kebab-case
+          '**/*.{ts,tsx}': 'KEBAB_CASE',
+        },
+        {
+          ignoreMiddleExtensions: true, // Пропускає крапки у файлах на кшталт auth.types.ts або button.test.tsx
+        },
+      ],
+      'check-file/folder-naming-convention': [
+        'error',
+        {
+          // Усі папки без винятку теж у нижньому регістрі з дефісами
+          'src/**/': 'KEBAB_CASE',
+        },
+      ],
+    },
+  },
+
   eslintConfigPrettier,
 ]);
