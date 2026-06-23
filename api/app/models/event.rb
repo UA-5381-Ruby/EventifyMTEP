0e6dc742c6a401a93ff57d455a896c8e42ae7c72
tania-macheieva <macheieva.tetiana@gmail.com>
2026-06-23 07:20:09 +0000

    refactor(api): extract hardcoded strings into I18n locale files models

--- api/app/models/event.rb
+++ api/app/models/event.rb
@@ -91,10 +91,9 @@ class Event < ApplicationRecord
 
   def end_date_after_start_date
     return if end_date.blank? || start_date.blank?
-
     return unless end_date < start_date
 
-    errors.add(:end_date, 'must be after start date')
+    errors.add(:end_date, :after_start_date)
   end
 
   validates :title, presence: true, length: { maximum: 120 }
