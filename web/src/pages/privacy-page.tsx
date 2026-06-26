a69520facd9a5cee026a048237c199ea3358a1ae
tania-macheieva <macheieva.tetiana@gmail.com>
2026-06-26 11:47:15 +0000

    refactor: double quotes to single

--- web/src/pages/privacy-page.tsx
+++ web/src/pages/privacy-page.tsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 import { Badge } from '@/components/ui';
 import { Container, PageWrapper } from '@/components/layout';
 
