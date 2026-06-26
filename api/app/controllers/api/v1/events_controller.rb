dae854ebb2552ee031fd410796d9f14d35e95c58
maxim <167625699+RiXeN12@users.noreply.github.com>
2026-06-26 14:36:56 +0300

    synchronize package.json and package-lock.json

--- api/app/controllers/api/v1/events_controller.rb
+++ api/app/controllers/api/v1/events_controller.rb
@@ -14,23 +14,20 @@ module Api
         render json: { errors: [e.message] }, status: :unprocessable_content
       end
 
+      rescue_from ActiveRecord::RecordNotFound do
+        render json: { error: t('api.v1.errors.brands.not_found_or_access_denied') }, status: :forbidden
+      end
+
       before_action :require_authentication!, only: %i[create update]
       before_action :set_event, only: %i[show update]
 
       def index
         paginated = paginate(EventFilter.new(index_params, current_user).call)
-
-        render json: {
-          data: paginated[:records].as_json(methods: [:banner_url]),
-          meta: paginated[:meta]
-        }
+        render json: { data: paginated[:records].as_json(methods: [:banner_url]), meta: paginated[:meta] }
       end
 
       def show
-        render json: @event.as_json(
-          methods: [:banner_url],
-          include: event_serialization_includes
-        ), status: :ok
+        render json: @event.as_json(methods: [:banner_url], include: event_serialization_includes), status: :ok
       end
 
       def create
@@ -40,41 +37,30 @@ module Api
         else
           render json: { errors: @event.errors }, status: :unprocessable_content
         end
-      rescue ActiveRecord::RecordNotFound
-        render json: { error: t('api.v1.errors.brands.not_found_or_access_denied') }, status: :forbidden
       end
 
       def update
         authorize_brand_access!(@event.brand_id)
 
         if @event.update(update_event_attrs)
-          render json: @event.as_json(
-            methods: [:banner_url],
-            include: event_serialization_includes
-          ), status: :ok
+          render json: @event.as_json(methods: [:banner_url], include: event_serialization_includes), status: :ok
         else
           render json: { errors: @event.errors }, status: :unprocessable_content
         end
-      rescue ActiveRecord::RecordNotFound
-        render json: { error: t('api.v1.errors.brands.not_found_or_access_denied') }, status: :forbidden
       end
 
       private
 
       def build_event
         brand = authorize_brand_access!(event_params[:brand_id])
-        attrs = process_banner_upload(event_base_params.to_h)
-
-        Event.new(attrs.merge(brand: brand, status: 'draft')).tap do |event|
+        Event.new(process_banner_upload(event_base_params.to_h).merge(brand: brand, status: 'draft')).tap do |event|
           event.category_ids = event_params[:category_ids] if event_params[:category_ids].present?
         end
       end
 
       def update_event_attrs
         attrs = process_banner_upload(event_base_params.to_h)
-
         attrs.merge!(category_ids: event_params[:category_ids]) if event_params[:category_ids].present?
-
         attrs
       end
 
@@ -83,28 +69,21 @@ module Api
         file = attrs[:banner]
 
         if file.present? && file.is_a?(ActionDispatch::Http::UploadedFile)
-          attrs[:banner] =
-            validated_media_key(file, folder: 'events/banners',
-                                      error_scope: 'api.v1.errors.events.banner')
+          attrs[:banner] = validated_media_key(file, 'api.v1.errors.events.banner')
         end
-
         attrs
       end
 
-      def validated_media_key(file, folder:, error_scope:)
+      def validated_media_key(file, error_scope)
         raise MediaUploadError, t("#{error_scope}.invalid_format") unless file.content_type.in?(ALLOWED_BANNER_TYPES)
         raise MediaUploadError, t("#{error_scope}.too_large", max_size: '5MB') if file.size > MAX_BANNER_SIZE
 
-        s3_key = S3BucketService.new.upload(file, folder: folder)
-        raise MediaUploadError, t("#{error_scope}.upload_failed") if s3_key.nil?
-
-        s3_key
+        S3BucketService.new.upload(file, folder: 'events/banners') || raise(MediaUploadError, t("#{error_scope}.upload_failed"))
       end
 
       def authorize_brand_access!(brand_id = event_params[:brand_id])
         brand = current_user.brands.find(brand_id)
         membership = brand.brand_memberships.find_by(user_id: current_user.id)
-
         raise Pundit::NotAuthorizedError unless membership&.role.in?(%w[owner manager])
 
         brand
@@ -123,23 +102,16 @@ module Api
       end
 
       def event_serialization_includes
-        {
-          brand: { only: %i[id name] },
-          categories: { only: %i[id name] }
-        }
+        { brand: { only: %i[id name] }, categories: { only: %i[id name] } }
       end
 
       def event_params
-        params.expect(event: [
-                        :title, :description, :location, :start_date,
-                        :end_date, :status, :brand_id, :banner, :price_cents, :available_tickets_count,
-                        { category_ids: [] }
-                      ])
+        params.expect(event: [:title, :description, :location, :start_date, :end_date, :status,
+                              :brand_id, :banner, :price_cents, :available_tickets_count, { category_ids: [] }])
       end
 
       def index_params
-        @index_params ||= params.permit(:from, :to, :q, :sort, :order, :page, :per_page, :status, :brand_id,
-                                        :category_id)
+        @index_params ||= params.permit(:from, :to, :q, :sort, :order, :page, :per_page, :status, :brand_id, :category_id)
       end
     end
   end
